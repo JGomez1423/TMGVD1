3b0199434b9b86be13dcb7d07b85a3d57d4cd58e
Acoc01 <63213116+Acoc01@users.noreply.github.com>
2022-08-20 18:57:25 -0400

    def

--- limpiar.py
+++ limpiar.py
@@ -3,10 +3,8 @@ f = open(str(filename),"r")
 content = f.read()
 content.upper();
 final_content = content.translate({ ord(c): None for c in "N>" })
-f2 = open(str(filename)+str(2), "w")
-for line in content:
-    line.upper()
-f2.write(content)
+f2 = open("test2.txt","w")
+f2.write(final_content.upper())
 f2.close()
 f.close()
 #print(content)
