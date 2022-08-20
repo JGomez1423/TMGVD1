5ec02990ca8a08274b77ede5bafee57e280bb155
Acoc01 <63213116+Acoc01@users.noreply.github.com>
2022-08-20 18:27:34 -0400

    Update limpiar.py

--- limpiar.py
+++ limpiar.py
@@ -3,8 +3,10 @@ f = open(str(filename),"r")
 content = f.read()
 content.upper();
 final_content = content.translate({ ord(c): None for c in "N>" })
-f2 = open(str(filename), "w")
-f2.write(str(final_content))
+f2 = open(str(filename)+str(2), "w")
+for line in content:
+    line.upper()
+f2.write(content)
 f2.close()
 f.close()
 #print(content)
